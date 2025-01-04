4b5ee07338053ef4b4efcca390cd5e7d5a0d7495
Aritra <aritra.2003k@gmail.com>
2025-01-04 21:07:29 +0530

    Updated stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -18,6 +18,7 @@ int main(){
     //Unlike Vector, List or array or any other container, stack is not a container.
     //Stack is a container adaptor. Its main task is to perform stack based operations i.e. push, pop or view top element of any underlying actual container.
     //By default a std::deque is implemented as the base container for std::stack
+    //Stack follow the LIFO method [ Last In First Out ];
 
     s.push(7);    
     s.push(9);    
@@ -30,7 +31,8 @@ int main(){
     v_stack.push("World");
     std::cout<< v_stack.top() << std::endl;
 
-    std::cout<< v_stack.size() << std::endl; //returns the number of elements in the stack
+    std::cout<< "Number of elements in the stack: " <<v_stack.size() << std::endl; //returns the number of elements in the stack
+    std::cout<< "Size of stack: " << sizeof(v_stack) << std::endl; //returns the size of the stack object
 
     //type mismatch //!!! DO NOT DO THIS !!!
     //std::stack<std::string, std::vector<float>> mismatch_stack; //stack of type string but using float type vector as container
